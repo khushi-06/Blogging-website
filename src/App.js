615552ea47ebce0e06485c289f4211fe76ec7bce
khushi gupta <guptakhushi622003@gmail.com>
2023-08-14 01:23:55 +0530

    added the basic react template and the Navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Navbar from './componets/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      
+    </div>
+  );
+}
+
+export default App;
