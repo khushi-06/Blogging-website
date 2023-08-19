c16709393bbc8acf384f966feb7be31b6e414eb6
khushi gupta <guptakhushi622003@gmail.com>
2023-08-20 00:50:18 +0530

    fixed the error in the connection

--- server/database/db.js
+++ server/database/db.js
@@ -1,24 +1,19 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
+const Connection = (USERNAME, PASSWORD) => {
+  // Use backticks to create a template string
+  const URI = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.fd2wuzh.mongodb.net/?retryWrites=true&w=majority`;
 
-const Connection = ()=>{
-
-// Import required modules using ES module syntax
-
-// Replace 'YOUR_MONGODB_URI' with your actual MongoDB URI
-const URI = 'mongodb+srv://guptakhushi622003:mogodb-khushi@cluster0.fd2wuzh.mongodb.net/?retryWrites=true&w=majority';
-
-mongoose.connect(URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(() => {
-    console.log('Finally Connected to the database');
+  mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
-  .catch((error) => {
-    console.error('Error connecting to the database:', error);
-  });
-}
+    .then(() => {
+      console.log('Connected to the database');
+    })
+    .catch((error) => {
+      console.error('Error connecting to the database:', error);
+    });
+};
 
-
-export default Connection
+export default Connection;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import Connection from './database/db.js';
+
+
+dotenv.config();
 
 const app = express();
 
 const PORT =8000;
 app.listen(PORT ,()=>console.log(`server running on  ${PORT}`));
 
-Connection();
+const USERNAME=process.env.DB_USERNAME;
+const PASSWORD=process.env.DB_PASSWORD
+
+Connection(USERNAME,PASSWORD);
