9c60f83e5d614d3b1c30e70ec6dd6f7e8a9c4826
khushi gupta <guptakhushi622003@gmail.com>
2023-08-19 23:59:08 +0530

    Eshtablished The mongo db connection on cloud

--- server/database/db.js
+++ server/database/db.js
@@ -1,8 +1,24 @@
-export const Connection =()=>{
-    try{
-        
-        
-    }catch(error){
-        
-    }
+import mongoose from "mongoose"
+
+
+const Connection = ()=>{
+
+// Import required modules using ES module syntax
+
+// Replace 'YOUR_MONGODB_URI' with your actual MongoDB URI
+const URI = 'mongodb+srv://guptakhushi622003:mogodb-khushi@cluster0.fd2wuzh.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('Finally Connected to the database');
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error);
+  });
 }
+
+
+export default Connection
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import Connection from './database/db.js';
 
 const app = express();
 
 const PORT =8000;
 app.listen(PORT ,()=>console.log(`server running on  ${PORT}`));
+
+Connection();
